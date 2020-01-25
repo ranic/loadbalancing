7000a013ccaf8e8093b766919a0092a21a867d58
Vijay Jayaram <vijay.jayaram@mixpanel.com>
2020-01-25 13:53:15 -0600

    denser statistics

--- main.py
+++ main.py
@@ -11,20 +11,20 @@ def stats(arr):
 
 # Work cost distributions (returns an integer >= 1)
 
-def uniform():
+def one():
     return 1
 
 def powerlaw():
     return floor(random.paretovariate(5))
 
 
-def uniformly_random(N, costfn):
+def uniform(N, costfn):
     bins = [0.0 for _ in xrange(N)]
     for _ in xrange(N):
         bins[random.randint(0, N-1)] += costfn()
     return stats(bins)
 
-def power_of_2(N, costfn):
+def pow2(N, costfn):
     bins = [0.0 for _ in xrange(N)]
     for _ in xrange(N):
         a = random.randint(0, N-1)
@@ -36,10 +36,11 @@ def power_of_2(N, costfn):
             bins[b] += x
     return stats(bins)
 
-row_format ="{:>10}" * 6
-for fn in [uniformly_random, power_of_2]:
-    for costfn in [uniform, powerlaw]:
-        print "Testing {} balancing with {} cost function".format(fn.__name__, costfn.__name__)
-        print row_format.format("N", "min", "p25", "p50", "p75", "max")
-        for N in [10, 100, 1000, 10000, 100000]:
-            print row_format.format(N, *fn(N, costfn))
+
+N = 100 * 1000
+print "Testing with {} bins and balls".format(N)
+row_format ="{:>12}" * 7
+print row_format.format("balancing_fn", "cost_fn", "min", "p25", "p50", "p75", "max")
+for distfn in [uniform, pow2]:
+    for costfn in [one, powerlaw]:
+        print row_format.format(distfn.__name__, costfn.__name__, *distfn(N, costfn))
